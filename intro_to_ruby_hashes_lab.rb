b8bae4c6afa5aae880c112ac0cef17ba9a293823
Josh Couper <joshcouper@gmail.com>
2020-01-27 00:50:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,7 @@ end
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
   
-  my hash = :name => "Grace Hopper"
+  my_hash = :name => "Grace Hopper"
 end
 
 def id_generator
